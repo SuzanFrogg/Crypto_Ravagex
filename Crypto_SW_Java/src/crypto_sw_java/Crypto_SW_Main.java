dc24e0cae22d92fd4328dba1f50c0f71a190cb6d
SuzanFrogg <33319686+SuzanFrogg@users.noreply.github.com>
2021-02-22 11:09:09 +0100

    Update Crypto_SW_Main.java

--- Crypto_SW_Java/src/crypto_sw_java/Crypto_SW_Main.java
+++ Crypto_SW_Java/src/crypto_sw_java/Crypto_SW_Main.java
@@ -1,5 +1,10 @@
 package crypto_sw_java;
 
+import Communication.Client;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * 
  * @author Thibault
@@ -10,7 +15,16 @@ public class Crypto_SW_Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Bouh");
+       
+        try {
+            Client client = new Client();
+            client.connexion();
+            client.creationFlux();
+            client.boucleDeDiscussion();
+        } catch (IOException ex) {
+            Logger.getLogger(Crypto_SW_Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
     
 }
